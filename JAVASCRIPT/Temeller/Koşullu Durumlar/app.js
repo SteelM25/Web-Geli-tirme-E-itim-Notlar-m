d376c1400bc8071041dab7265f0d91a675f66aa1
SteelM25 <cmusa4031@gmail.com>
2024-08-16 10:17:27 +0300

    iframe

---
+++ JAVASCRIPT/Temeller/Koşullu Durumlar/app.js
@@ -0,0 +1,15 @@
+/*
+    ? Karşılaştırma Operatörleri
+
+    == İki taraf birbirine eşit mi
+    === İki tarafın hem değeri hem veri tipi birbirine eşit mi
+    != İki taraf birbirine eşit değilse
+    !== İki tarafın hem değerleri hem de veri tipi birbirine eşit değilse
+    > Büyükse
+    < Küçükse
+    >= Büyük veya Eşitse
+    <= Küçük veya Eşitse
+
+    & ve (her iki durum da sağlanmalı)
+    || veya (bir durum sağlanması yeterli)
+*/
